952a50367f47d5921712514401d7ddabcf3783c0
sxpydo <shavypeynado@gmail.com>
2025-06-13 21:42:31 +0100

    Created a new console project using dotnet new console.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Connect Four Kata - Start here!");
+        // Add game logic here as you progress
+    }
+}
